a92450599240245e663e2ad7a7172e90c22b9835
Mykhailo Levndovskyi <mlevandovskyi@softjourn.com>
2023-03-27 16:27:57 +0300

    save

--- src/app/css-group/index.ts
+++ src/app/css-group/index.ts
@@ -1,13 +1,14 @@
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 
-import {CssGroupComponent} from './css-group.component';
-import {FormsModule} from "@angular/forms";
-import {CommonModule} from "@angular/common";
-import {MatInputModule} from "@angular/material/input";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatCheckboxModule} from "@angular/material/checkbox";
-import {SelectTemplateModule} from "../select-template";
+import { CssGroupComponent } from './css-group.component';
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { SelectTemplateModule } from "../select-template";
 import { MatExpansionModule } from '@angular/material/expansion';
+import { SelectColorModule } from '@app/controls/select-color';
 
 @NgModule({
   declarations: [
@@ -16,15 +17,16 @@ import { MatExpansionModule } from '@angular/material/expansion';
   exports: [
     CssGroupComponent
   ],
-        imports: [
-                FormsModule,
-                CommonModule,
-                MatInputModule,
-                MatFormFieldModule,
-                MatCheckboxModule,
-                SelectTemplateModule,
-                MatExpansionModule
-        ],
+  imports: [
+    FormsModule,
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    SelectTemplateModule,
+    MatExpansionModule,
+    SelectColorModule
+  ],
   providers: [],
 })
 export class CssGroupModule {
